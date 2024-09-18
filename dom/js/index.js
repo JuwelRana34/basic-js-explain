2f8c09db9ce12e132afec707d1a4e7d5b61f8775
JuwelRana34 <juwelrana3426@gmail.com>
2024-09-18 13:04:32 +0600

    update dom

--- dom/js/index.js
+++ dom/js/index.js
@@ -1,29 +1,38 @@
 const parent = document.getElementById("parent");
 const input = document.getElementById("input");
 const btn = document.getElementById("btn");
-const demotext = document.getElementById('demotext')
+const demotext = document.getElementById('demotext');
+const clr = document.getElementById('clr')
 btn.addEventListener("click", () => {
   const value = input.value;
   const li = document.createElement("li");
   li.setAttribute("class"," p-3 text-black bg-gray-100 rounded-md my-2 hover:bg-blue-200 transition-all hover:cursor-pointer flex flex-row justify-between")
   const deleteBtn = document.createElement("button");
   deleteBtn.innerText = "delete";
-  deleteBtn.setAttribute('class', 'py-2 px-5 bg-red-300 rounded-md text-red-500 mx-2 hover:bg-red-400 hover:text-red-600 font-semibold font-lg ')
+  deleteBtn.setAttribute('class', 'py-2 px-5 bg-red-400 rounded-md text-white mx-2 hover:bg-red-400 hover:text-red-600 font-semibold font-lg ')
 
   deleteBtn.addEventListener("click", (e) => {
-    const child = e.target.parentElement;
-    e.target.parentNode.parentNode.removeChild(child);
+    // const child = e.target.parentElement;
+    e.target.parentNode.parentNode.removeChild(e.target.parentElement);
   });
   li.innerText = value;
   if (value !== "") {
     parent.appendChild(li);
     li.appendChild(deleteBtn);
+  }else{
+    alert('please add some task')
   }
 
   input.value = "";
 
  
 });
+
+clr.addEventListener('click',()=>{
+   
+   console.log(parent.innerHTML = '');
+})
+
 
 // parent.addEventListener( 'click', (e) => {
 //      e.target.parentNode.removeChild(e.target)
@@ -67,6 +76,8 @@ inputCoupon.addEventListener("keyup", ()=>{
   if(inputCoupon.value == coupon){
     
     show.innerText = " 🥳 You have got successfully ✅50% discount"
+  }else{
+    show.innerText = ""
   }
   
 })
